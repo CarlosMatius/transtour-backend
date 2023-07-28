6cacdf82815958c49966773cc0026fd897247732
Carlos Mateus <carlosmatius.9326@gmail.com>
2023-07-27 21:10:42 -0500

    inicio del proyecto

---
+++ src/main/java/com/transtour/backend/TranstourBackendApplication.java
@@ -0,0 +1,13 @@
+package com.transtour.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranstourBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranstourBackendApplication.class, args);
+	}
+
+}
