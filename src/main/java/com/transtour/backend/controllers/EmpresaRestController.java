5b109fb30ee8ff0574066c5ce423c7daf98554e0
Carlos Mateus <carlosmatius.9326@gmail.com>
2023-08-12 04:05:35 -0500

    agregada la subida de imagenes

--- src/main/java/com/transtour/backend/controllers/EmpresaRestController.java
+++ src/main/java/com/transtour/backend/controllers/EmpresaRestController.java
@@ -1,17 +1,29 @@
 package com.transtour.backend.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -23,7 +35,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.transtour.backend.models.dto.EmpresaDTO;
 import com.transtour.backend.models.services.IEmpresaService;
@@ -35,6 +49,8 @@ public class EmpresaRestController {
 
 	private static final String MESSAGE = "message";
 	private static final String ERROR = "error";
+	private static final String CARPETA = "uploads";
+	private final Logger log = LoggerFactory.getLogger(EmpresaRestController.class);
 	
 	@Autowired
 	private IEmpresaService empresaService;
@@ -132,11 +148,21 @@ public class EmpresaRestController {
 	}
 	
 	@DeleteMapping("/empresas/{id}")
-	public ResponseEntity<Object> delete(@PathVariable Long id) {
+	public ResponseEntity<Object> delete(@PathVariable Long id) throws IOException {
 		
 		Map<String, Object> response = new HashMap<>();
 		
 		try {
+			EmpresaDTO empresaDTO = empresaService.findById(id);
+			String nombreFotoAnterior = empresaDTO.getImagen();
+			
+			if(nombreFotoAnterior !=null && nombreFotoAnterior.length() > 0) {
+				Path rutaFotoAnterior = Paths.get(CARPETA).resolve(nombreFotoAnterior).toAbsolutePath();
+				File archivoFotoAnterior = rutaFotoAnterior.toFile();
+				if(archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+					Files.delete(rutaFotoAnterior);
+				}
+			}
 			empresaService.delete(id);
 		} catch (DataAccessException e) {
 			response.put(MESSAGE, "No se pudo eliminar la empresa en la base de datos");
@@ -147,5 +173,81 @@ public class EmpresaRestController {
 		response.put(MESSAGE, "La empresa ha sido eliminada exitosamente!");
 		
 		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+	
+	@PostMapping("/empresas/upload")
+	public ResponseEntity<Object> upload(@RequestParam("archivo") MultipartFile archivo, @RequestParam("archivo") Long id) {
+		Map<String, Object> response = new HashMap<>();
+		
+		EmpresaDTO empresaDTO = empresaService.findById(id);
+		String ruta = archivo.getOriginalFilename();
+		
+		if(!archivo.isEmpty() && ruta !=null) {
+			String nombreArchivo = UUID.randomUUID().toString() + "_" + ruta.replace(" ", "");
+			Path rutaArchivo = Paths.get(CARPETA).resolve(nombreArchivo).toAbsolutePath();
+			log.info("{}", rutaArchivo);
+			try {
+				Files.copy(archivo.getInputStream(), rutaArchivo);
+			} catch (IOException e) {
+				response.put(MESSAGE, "Error al subir la imagen de la empresa");
+				response.put(ERROR, e.getMessage() +": "+ e.getCause().getMessage());
+				return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+			String nombreFotoAnterior = empresaDTO.getImagen();
+			
+			if(nombreFotoAnterior !=null && nombreFotoAnterior.length() > 0) {
+				Path rutaFotoAnterior = Paths.get(CARPETA).resolve(nombreFotoAnterior).toAbsolutePath();
+				File archivoFotoAnterior = rutaFotoAnterior.toFile();
+				if(archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+					try {
+						Files.delete(rutaFotoAnterior);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			empresaDTO.setImagen(nombreArchivo);
+			
+			empresaService.save(empresaDTO);
+			
+			response.put("empresa", empresaDTO);
+			response.put(MESSAGE, "Has subido correctamente la imagen: " + nombreArchivo);
+		}
+		
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/empresas/uploads/img/{nombreFoto:.+}")
+	public ResponseEntity<Resource> verFoto(@PathVariable String nombreFoto) {
+		Path rutaArchivo = Paths.get(CARPETA).resolve(nombreFoto).toAbsolutePath();
+		log.info("{}", rutaArchivo);
+		HttpHeaders cabecera = new HttpHeaders();
+		Resource recurso = null;
+		try {
+			recurso = new UrlResource(rutaArchivo.toUri());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		if(recurso !=null) {
+			if(!recurso.exists() && !recurso.isReadable()) {
+				throw new ErrorExceptionUpload(nombreFoto);
+			}
+			
+			cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"");
+		}
+		
+		return new ResponseEntity<>(recurso, cabecera, HttpStatus.OK);
+	}
+	
+	// excepcion personalizada
+	private class ErrorExceptionUpload extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+
+		public ErrorExceptionUpload(String parametro) {
+			super("Error no se pudo cargar la imagen:" + parametro);
+		}
 	}
 }
